4f3094576d9cde802d0832dbac667ec38fd0d688
GEARdHAX <adarsharya2911@gmail.com>
2025-09-30 14:04:55 +0530

    Deleted Old files

--- src/DiseaseDetection.jsx
+++ src/DiseaseDetection.jsx
@@ -28,7 +28,7 @@ function DiseaseDetectionPage() {
     
     // --- 2. CONNECT TO WEBSOCKET SERVER ---
     // TODO: Replace with your actual server IP address
-    const serverUrl = "http://172.25.25.112:8080"; 
+    const serverUrl = "http://10.44.189.237:8080"; 
     socketRef.current = io(serverUrl);
 
     socketRef.current.on('connect', () => {
